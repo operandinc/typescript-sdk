c946bb5df7eed35a8c467d985b805b3de2b04f37
Donwauchopex <donwauchopex@gmail.com>
2022-04-10 00:13:01 -0400

    Added my project

---
+++ src/sdk.ts
@@ -0,0 +1,168 @@
+import {
+  Collection,
+  Group,
+  Source,
+  GroupMetadata,
+  SearchResponse,
+} from './types';
+import axios from 'axios';
+
+const baseUrl = 'https://core.operand.ai/';
+
+export class Operand {
+  private apiKey: string;
+  public endpoint: string;
+
+  constructor(apiKey: string, endpoint?: string) {
+    this.apiKey = apiKey;
+    this.endpoint = endpoint || baseUrl;
+  }
+
+  // COLLECTIONS
+
+  // Get a collection by id
+  async getCollection(collectionId: string): Promise<Collection> {
+    let url = `${this.endpoint}/collection/${collectionId}`;
+    let response = await axios.get(url, {
+      headers: {
+        Authorization: `${this.apiKey}`,
+      },
+    });
+    return response.data as Collection;
+  }
+
+  // List collections
+  async listCollections(
+    limit?: number,
+    offset?: number
+  ): Promise<{
+    collections: Collection[];
+    more: boolean;
+  }> {
+    let url = `${this.endpoint}/collection`;
+    let response = await axios.get(url, {
+      params: {
+        limit: limit,
+        offset: offset,
+      },
+      headers: {
+        Authorization: `${this.apiKey}`,
+      },
+    });
+    return response.data as { collections: Collection[]; more: boolean };
+  }
+
+  // Create a collection
+  async createCollection(source: Source): Promise<Collection> {
+    let url = `${this.endpoint}/collection`;
+    let response = await axios.post(
+      url,
+      { source: source.source, metadata: source.metadata },
+      {
+        headers: {
+          Authorization: `${this.apiKey}`,
+        },
+      }
+    );
+    return response.data as Collection;
+  }
+
+  // Delete a collection
+  async deleteCollection(collectionId: string): Promise<{ deleted: boolean }> {
+    let url = `${this.endpoint}/collection/${collectionId}`;
+    let response = await axios.delete(url, {
+      headers: {
+        Authorization: `${this.apiKey}`,
+      },
+    });
+    return response.data as { deleted: boolean };
+  }
+
+  // GROUPS
+
+  // Get a group by id
+  async getGroup(groupId: string): Promise<Group> {
+    let url = `${this.endpoint}/group/${groupId}`;
+    let response = await axios.get(url, {
+      headers: {
+        Authorization: `${this.apiKey}`,
+      },
+    });
+    return response.data as Group;
+  }
+
+  // List groups
+  async listGroups(
+    collection?: string,
+    limit?: number,
+    offset?: number
+  ): Promise<{
+    groups: Group[];
+    more: boolean;
+  }> {
+    let url = `${this.endpoint}/group`;
+    let response = await axios.get(url, {
+      params: {
+        limit: limit,
+        offset: offset,
+        collection: collection,
+      },
+      headers: {
+        Authorization: `${this.apiKey}`,
+      },
+    });
+    return response.data as { groups: Group[]; more: boolean };
+  }
+
+  // Create a group
+  async createGroup(
+    collectionId: string,
+    metadata: GroupMetadata
+  ): Promise<Group> {
+    let url = `${this.endpoint}/group`;
+    let response = await axios.post(
+      url,
+      { collectionId, ...metadata },
+      {
+        headers: {
+          Authorization: `${this.apiKey}`,
+        },
+      }
+    );
+    return response.data as Group;
+  }
+
+  // Delete a group
+  async deleteGroup(groupId: string): Promise<{ deleted: boolean }> {
+    let url = `${this.endpoint}/group/${groupId}`;
+    let response = await axios.delete(url, {
+      headers: {
+        Authorization: `${this.apiKey}`,
+      },
+    });
+    return response.data as { deleted: boolean };
+  }
+
+  // Search
+  async search(
+    collections: string[],
+    query: string,
+    limit?: number
+  ): Promise<SearchResponse> {
+    let url = `${this.endpoint}/search`;
+    let response = await axios.post(
+      url,
+      {
+        collections,
+        query,
+        limit,
+      },
+      {
+        headers: {
+          Authorization: `${this.apiKey}`,
+        },
+      }
+    );
+    return response.data as SearchResponse;
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,70 @@
+// A single collection
+export type Collection = {
+  id: string;
+  createdAt: number;
+  updatedAt: number;
+  source: string;
+  metadata: any;
+  status: Status;
+};
+
+export type Status = 'indexing' | 'ready';
+
+export type Group = {
+  id: string;
+  collectionId: string;
+  createdAt: number;
+  kind: string;
+  metadata: any;
+};
+
+export type Atom = {
+  id: string;
+  groupId: string;
+  createdAt: number;
+  content: string;
+  kind: AtomKind;
+};
+
+export type AtomKind = 'title' | 'content' | 'link' | 'image' | 'code';
+
+export type ScoredAtom = Atom & {
+  score: number;
+};
+
+export type SearchResponse = {
+  latencyMs: number;
+  atoms: ScoredAtom[];
+  groups?: {
+    [groupId: string]: Group;
+  };
+};
+
+export type GroupMetadata =
+  | {
+      kind: 'rss_item';
+      metadata: {
+        title?: string;
+        description?: string;
+        link: string;
+      };
+    }
+  | {
+      kind: 'html';
+      metadata: {
+        html: string;
+        title?: string;
+      };
+    };
+
+export type Source =
+  | {
+      source: 'rss';
+      metadata: {
+        url: string;
+      };
+    }
+  | {
+      source: 'none';
+      metadata: {};
+    };
