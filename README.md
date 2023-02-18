c9d9471ce33a8d795c64ff2edb34f641ac9307a8
Morgan Gallant <morgan@morgangallant.com>
2023-02-18 02:04:02 -0500

    hi
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Operand SDK
 
-The official Typescript SDK for the [Operand](https://beta.operand.ai) API. The SDK itself is generated against our [public API](https://buf.build/operand/mcp). If you're interested in learning more about this API, please visit [the landing page](https://beta.operand.ai/developers).
+The official Typescript SDK for the [Operand](https://beta.operand.ai) API. Read more on our [docs](https://docs.operand.ai).
--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,51 @@ import {
   createConnectTransport,
   createPromiseClient,
   Interceptor,
+  PromiseClient,
+  StreamResponse,
   Transport,
+  UnaryResponse,
 } from '@bufbuild/connect-web';
-import type { ServiceType } from '@bufbuild/protobuf';
-import type { CreateFileResponse } from './file/v1/file_pb';
+import type {
+  AnyMessage,
+  JsonValue,
+  Message,
+  MethodInfo,
+  PartialMessage,
+  ServiceType,
+} from '@bufbuild/protobuf';
+import 'isomorphic-fetch';
+import type { Properties } from './file/v1/file_pb';
 
+// A utility function to upload a file.
 export async function uploadFile(
-  parentId: string,
-  name: string,
   apiKey: string,
-  file?: File
+  name: string,
+  parentId: string, // If "", then the file is uploaded to the root directory.
+  file?: ArrayBuffer,
+  properties?: Properties,
 ) {
-  const url = 'https://mcp.operand.ai/upload';
-  const formData = new FormData();
-  if (file) {
-    formData.append('file', file);
+  const data = new FormData();
+  data.append("name", name);
+  data.append("parent_id", parentId);
+  if (properties) {
+    data.append("properties", properties.toJsonString());
   }
-  formData.append('parent_id', parentId);
-  formData.append('name', name);
-  // Multipart form data
-  const response = await fetch(url, {
+  if (file) {
+    data.append("file", new Blob([file]));
+  }
+  const response = await fetch("https://mcp.operand.ai/upload", {
+    method: "POST",
     headers: {
-      Authorization: 'Key ' + apiKey,
+      Authorization: "Key " + apiKey,
     },
-    method: 'POST',
-    body: formData,
+    body: data,
   });
-  const json = (await response.json()) as CreateFileResponse;
-  return json;
+  const text = await response.text();
+  if (!response.ok) {
+    throw new Error(text);
+  }
+  return JSON.parse(text);
 }
 
 function createHeaderInterceptor(headers: {
@@ -47,38 +64,106 @@ function createHeaderInterceptor(headers: {
   };
 }
 
-export const developInProd = true;
-
-export async function operandClient<T extends ServiceType>(
+export function operandClient<T extends ServiceType>(
   service: T,
   apiKey: string,
-  extraHeaders?: { [key: string]: string | null }
-) {
-  const baseUrl = 'https://mcp.operand.ai';
-
-  var transport: Transport;
-
-  transport = createConnectTransport({
-    baseUrl,
-    interceptors: [
-      createHeaderInterceptor({
-        ...extraHeaders,
-        Authorization: 'Key ' + apiKey,
-      }),
-    ],
-    jsonOptions: {
-      ignoreUnknownFields: true,
-    },
-  });
-
+  endpoint?: string,
+  extraHeaders?: { [key: string]: string | null },
+  forceFetchTransport?: boolean
+): PromiseClient<T> {
+  const baseUrl = endpoint || 'https://mcp.operand.ai';
+  const headers = {
+    ...extraHeaders,
+    Authorization: `Key ${apiKey}`,
+  };
+  const transport =
+    hasFetchApi() && !forceFetchTransport
+      ? createConnectTransport({
+          baseUrl: baseUrl,
+          interceptors: [createHeaderInterceptor(headers)],
+          jsonOptions: {
+            ignoreUnknownFields: true,
+          }
+        })
+      : createNodeFetchTransport(baseUrl, headers);
   return createPromiseClient(service, transport);
 }
 
-export * from './file/v1/file_pb';
-export * from './file/v1/file_connectweb';
+function hasFetchApi(): boolean {
+  try {
+    new Headers();
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
 
-export * from './operand/v1/operand_pb';
-export * from './operand/v1/operand_connectweb';
+function createRequestBody<T extends Message<T>>(message: T): BodyInit {
+  return message.toJsonString();
+}
 
-export * from './tenant/v1/tenant_pb';
-export * from './tenant/v1/tenant_connectweb';
+function createNodeFetchTransport(
+  baseUrl: string,
+  headers: { [key: string]: string | null }
+): Transport {
+  return {
+    async unary<
+      I extends Message<I> = AnyMessage,
+      O extends Message<O> = AnyMessage
+    >(
+      service: ServiceType,
+      method: MethodInfo<I, O>,
+      _signal: AbortSignal | undefined,
+      _timeoutMs: number | undefined,
+      _header: HeadersInit | undefined,
+      message: PartialMessage<I>
+    ): Promise<UnaryResponse<O>> {
+      const endpoint = `${baseUrl}/${service.typeName}/${method.name}`;
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          ...headers,
+          'Content-Type': 'application/json',
+        },
+        body: createRequestBody(
+          message instanceof method.I ? message : new method.I(message)
+        ),
+      });
+      if (!response.ok) {
+        const body = await response.text();
+        const message = `Request failed with code ${response.status}, status ${response.statusText}: ${body}`;
+        console.warn(message);
+        return Promise.reject(new Error(message));
+      }
+      return <UnaryResponse<O>>{
+        message: method.O.fromJson((await response.json()) as JsonValue, {
+          ignoreUnknownFields: true,
+        }),
+      };
+    },
+    async serverStream<
+      I extends Message<I> = AnyMessage,
+      O extends Message<O> = AnyMessage
+    >(
+      _service: ServiceType,
+      _method: MethodInfo<I, O>,
+      _signal: AbortSignal | undefined,
+      _timeoutMs: number | undefined,
+      _header: HeadersInit | undefined,
+      _message: PartialMessage<I>
+    ): Promise<StreamResponse<O>> {
+      return Promise.reject(new Error('Not implemented'));
+    },
+  };
+}
+
+export * from './file/v1/file_pb.js';
+export * from './file/v1/file_connectweb.js';
+
+export * from './operand/v1/operand_pb.js';
+export * from './operand/v1/operand_connectweb.js';
+
+export * from './tenant/v1/tenant_pb.js';
+export * from './tenant/v1/tenant_connectweb.js';
+
+export type { PartialMessage } from '@bufbuild/protobuf'; // Re-export for convenience.
